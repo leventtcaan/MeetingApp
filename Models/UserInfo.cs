d687432f48c1b393ce2f51d4838c250eb01ba8f2
Levent Can Ceylan <ceylanleventcan@gmail.com>
2026-04-25 17:25:42 +0300

    feat: implement data annotations and model state validation for user info form

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetingApp.Models
 {
     public class UserInfo
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Phone { get; set; }
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
-        public bool Participation { get; set; }
+        [Required]
+        public bool? Participation { get; set; }
 
     }
 }
